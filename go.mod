494a88b765e1f85165bb4e5954443e0ac2f307d9
SHOUJI Kazuo <shouji_kazuo@kurusugawa.jp>
2023-03-23 15:16:08 +0900

    first commit with gpt-4

---
+++ go.mod
@@ -0,0 +1,36 @@
+module github.com/shouji-kazuo/translate-html-go
+
+go 1.19
+
+require (
+	github.com/gocolly/colly/v2 v2.1.0
+	google.golang.org/api v0.114.0
+)
+
+require (
+	cloud.google.com/go/compute v1.18.0 // indirect
+	cloud.google.com/go/compute/metadata v0.2.3 // indirect
+	github.com/PuerkitoBio/goquery v1.5.1 // indirect
+	github.com/andybalholm/cascadia v1.2.0 // indirect
+	github.com/antchfx/htmlquery v1.2.3 // indirect
+	github.com/antchfx/xmlquery v1.2.4 // indirect
+	github.com/antchfx/xpath v1.1.8 // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.2.3 // indirect
+	github.com/googleapis/gax-go/v2 v2.7.1 // indirect
+	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
+	github.com/temoto/robotstxt v1.1.1 // indirect
+	go.opencensus.io v0.24.0 // indirect
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/oauth2 v0.6.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20230306155012-7f2fa6fef1f4 // indirect
+	google.golang.org/grpc v1.53.0 // indirect
+	google.golang.org/protobuf v1.29.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/url"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/gocolly/colly/v2"
+	"google.golang.org/api/googleapi/transport"
+	translate "google.golang.org/api/translate/v2"
+)
+
+var (
+	apiKey      = "YOUR_API_KEY" // Google Cloud Translation APIのAPIキーを入力
+	baseURL     string
+	outputDir   string
+	recursive   int
+	targetLang  = "ja"
+	visitedURLs = map[string]bool{}
+)
+
+func main() {
+	// 引数の定義
+	flag.StringVar(&outputDir, "output-dir", "./output", "The output directory for translated HTML/CSS files")
+	flag.StringVar(&baseURL, "url", "https://www.promptingguide.ai/", "The URL of the website to translate")
+	flag.IntVar(&recursive, "recursive", 1, "The number of levels to follow links from the base URL")
+	flag.Parse()
+
+	// 出力ディレクトリを作成
+	err := os.MkdirAll(outputDir, 0755)
+	if err != nil {
+		fmt.Println("Error creating output directory:", err)
+		return
+	}
+
+	// コレクターの作成
+	c := colly.NewCollector(
+		colly.MaxDepth(recursive),
+		colly.Async(true),
+	)
+
+	// 翻訳関数
+	translateFunc := func(text string) (string, error) {
+		ctx := context.Background()
+		client := translate.NewService(ctx, &transport.APIKey{Key: apiKey})
+
+		translations, err := client.Translations.List([]string{text}, targetLang).Do()
+		if err != nil {
+			return "", err
+		}
+
+		if len(translations.Translations) > 0 {
+			return translations.Translations[0].TranslatedText, nil
+		}
+
+		return "", fmt.Errorf("no translation found")
+	}
+
+	// スクレイピングの前処理
+	c.OnHTML("*", func(e *colly.HTMLElement) {
+		e.ForEach("*", func(_ int, e *colly.HTMLElement) {
+			text := strings.TrimSpace(e.Text)
+			if text != "" {
+				translatedText, err := translateFunc(text)
+				if err == nil {
+					e.Text = translatedText
+				} else {
+					fmt.Printf("Failed to translate text: %s\n", err)
+				}
+			}
+		})
+	})
+
+	// スクレイピング後の処理
+	c.OnScraped(func(r *colly.Response) {
+		outputPath := filepath.Join(outputDir, filepath.Base(r.Request.URL.Path))
+		err := ioutil.WriteFile(outputPath, []byte(r.Body), 0644)
+		if err != nil {
+			fmt.Printf("Failed to write file: %s\n", err)
+		} else {
+			fmt.Printf("Translated page saved to: %s\n", outputPath)
+		}
+	})
+	// リンクの処理
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Request.AbsoluteURL(e.Attr("href"))
+		// 同じドメインのURLのみクロール
+		if isSameDomain(baseURL, link) && !visitedURLs[link] {
+			visitedURLs[link] = true
+			err := e.Request.Visit(link)
+			if err != nil {
+				fmt.Printf("Error visiting link: %s\n", err)
+			}
+		}
+	})
+
+	// エラー処理
+	c.OnError(func(r *colly.Response, err error) {
+		fmt.Printf("Request URL: %s failed with response: %d\n", r.Request.URL, r.StatusCode)
+	})
+
+	// 開始URLの訪問
+	err = c.Visit(baseURL)
+	if err != nil {
+		fmt.Printf("Error visiting base URL: %s\n", err)
+	}
+
+	// 実行
+	c.Wait()
+}
+
+func isSameDomain(base, link string) bool {
+	baseDomain, err := url.Parse(base)
+	if err != nil {
+		return false
+	}
+	linkDomain, err := url.Parse(link)
+	if err != nil {
+		return false
+	}
+	return baseDomain.Host == linkDomain.Host
+}
